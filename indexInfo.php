8dfd62782160443591a8c023266faac8f4c19015
Negaihoshi <e222et@gmail.com>
2014-02-08 23:10:22 +0800

    update

--- indexInfo.php
+++ indexInfo.php
@@ -4,25 +4,22 @@
 <head>
     <meta charset="UTF-8">
 
-    <title>Course Management</title>
+    <title>TwinBox</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="Negaihoshi">
 
     <!--CSS-->
-    <link rel="stylesheet" href="css/uikit.gradient.min.css">
     <link rel="stylesheet" href="css/semantic.min.css">
     <!--Javascript-->
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-    <script src="js/uikit.min.js"></script>
     <script src="js/semantic.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.8/angular.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.8/angular-route.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.0/angular.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.0/angular-route.min.js"></script>
 
     <!--Customer-->
     <link href="css/style.css" rel="stylesheet">
     <script src="js/index.js"></script>
-    <script src="js/semanticCustomer.js"></script>
 </head>
